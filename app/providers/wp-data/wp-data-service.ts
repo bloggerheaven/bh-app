5525db60fd3f432ccdc8d3def31200b4e0b15373
Cyril Kyburz <kybi1995@gmail.com>
2016-09-17 14:34:52 +0200

    add category and tag service

--- app/providers/wp-data/wp-data-service.ts
+++ app/providers/wp-data/wp-data-service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../models/post';
 import { Member } from '../../models/member';
+import { Tag } from '../../models/tag';
+import { Category } from '../../models/category';
 import { WpDataMock } from './data/wp-data-mock';
 import 'rxjs/add/operator/map';
 
 /*
+
+  REFACTOR, it's not dry!!!!!!!!!!
+
   Generated class for the WpData provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
@@ -13,6 +18,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class WpDataService {
   constructor() {}
+
+  /* Post */
 
   getPosts(): Post[] {
     let posts = <Post[]> [];
@@ -32,6 +39,8 @@ export class WpDataService {
     return this.getPostsBy(key, value)[0];
   }
 
+  /* Member */
+
   getMembers(): Member[] {
     let members = <Member[]> [];
 
@@ -48,6 +57,46 @@ export class WpDataService {
 
   getFirstMemberBy(key: string, value: any): Member {
     return this.getMembersBy(key, value)[0];
+  }
+
+  /* Tag */
+
+  getTags(): Tag[] {
+    let tags = <Tag[]> [];
+
+    WpDataMock.getTags().forEach(function(tag: any) {
+      tags.push(new Tag(tag));
+    });
+
+    return tags;
+  }
+
+  getTagsBy(key: string, value: any): Tag[] {
+    return this.getTags().filter(function(tag: Tag) { return tag[key] === value; });
+  }
+
+  getFirstTagBy(key: string, value: any): Tag {
+    return this.getTagsBy(key, value)[0];
+  }
+
+  /* Category */
+
+  getCategories(): Category[] {
+    let categories = <Category[]> [];
+
+    WpDataMock.getCategories().forEach(function(category: any) {
+      categories.push(new Category(category));
+    });
+
+    return categories;
+  }
+
+  getCategoriesBy(key: string, value: any): Category[] {
+    return this.getCategories().filter(function(category: Category) { return category[key] === value; });
+  }
+
+  getFirstCategoryBy(key: string, value: any): Category {
+    return this.getCategoriesBy(key, value)[0];
   }
 }
 
