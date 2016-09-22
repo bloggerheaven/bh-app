b70d9aa912598a37d6d515bfd34840f2df33c4d1
Cyril Kyburz <kybi1995@gmail.com>
2016-09-22 13:52:05 +0200

    add signle tracking, only save missing

--- app/pages/single/single.ts
+++ app/pages/single/single.ts
@@ -1,17 +1,12 @@
-import { Component } from '@angular/core';
-import { SocialSharing } from 'ionic-native';
-import { NavController, NavParams } from 'ionic-angular';
-import { Post } from '../../models/post';
-import { Member } from '../../models/member';
-import { WpDataService } from '../../providers/wp-data/wp-data-service';
-import { MemberPage } from '../member/member';
+import {Component} from '@angular/core';
+import {SocialSharing} from 'ionic-native';
+import {NavController, NavParams} from 'ionic-angular';
+import {Post} from '../../models/post';
+import {Member} from '../../models/member';
+import {WpDataService} from '../../providers/wp-data/wp-data-service';
+import {MemberPage} from '../member/member';
+import {Duration} from "../../models/duration";
 
-/*
-  Generated class for the SinglePage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'build/pages/single/single.html',
   providers: [WpDataService]
@@ -19,11 +14,13 @@ import { MemberPage } from '../member/member';
 export class SinglePage {
   public post: Post;
   public member: Member;
+  private duration: Duration;
 
   constructor(private navCtrl: NavController, private params: NavParams, private wpDataService: WpDataService) {
     this.navCtrl = navCtrl;
     this.post = wpDataService.getFirstPostBy('id', this.params.get('postId'));
     this.member = this.wpDataService.getFirstMemberBy('id', this.post.member);
+    this.duration = new Duration;
   }
 
   pushMemberPage(memberId) {
@@ -38,5 +35,14 @@ export class SinglePage {
 
   share(post: Post) {
     SocialSharing.share(null, null, null, post.link);
+  }
+
+  onPageDidEnter() {
+    this.duration.start();
+  }
+
+  onPageWillLeave() {
+    this.duration.end();
+    console.log('Viewed post: ' + this.post.id + ' for: ' + this.duration.milliseconds() + ' ms');
   }
 }
